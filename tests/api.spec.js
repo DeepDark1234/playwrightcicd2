7a05fd411c68a6576748e457f05f6a6f91cf6c11
[deepdark] <kzkzjejs@gmail.com>
2024-09-23 11:05:27 +0700

    first commit

---
+++ tests/api.spec.js
@@ -0,0 +1,50 @@
+// @ts-check
+const { test, expect } = require('@playwright/test')
+
+test.setTimeout(6000000)
+
+var user_id
+
+test('Get Users', async ({ request }) => {
+  // การทดสอบ API Method Get
+  const response = await request.get('https://reqres.in/api/users/2')
+  console.log(response)
+
+  // การยืนยันหรือ Assertion
+  expect(response.status()).toBe(200)
+})
+
+test('Post Users', async ({ request }) => {
+  // การทดสอบ API Method Get
+  const response = await request.post('https://reqres.in/api/users', {
+    data: {
+      "name": "sam",
+      "job": "homeless"
+    }, headers: { "Accept": "application/json" }
+  })
+  console.log(response)
+
+  // การยืนยันหรือ Assertion
+  expect(response.status()).toBe(201)
+
+  var res = await response.json()
+  user_id = res.id
+})
+
+test('Update Users', async ({ request }) => {
+  // การทดสอบ API Method Get
+  const response = await request.put('https://reqres.in/api/users/2' + user_id, {
+    data: {
+      "name": "kongpipop the gay lord",
+      "job": "homeless"
+    }, headers: { "Accept": "application/json" }
+  })
+  expect(response.status()).toBe(200)
+})
+
+test('Delete Users', async ({ request }) => {
+  // การทดสอบ API Method Get
+  const response = await request.delete('https://reqres.in/api/users/2' + user_id)
+  expect(response.status()).toBe(204)
+})
+
